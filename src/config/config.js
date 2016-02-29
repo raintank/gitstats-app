7c56b8dc084f67c1059ce66c1d070d204689b9df
Anthony Woods <awoods@raintank.io>
2016-03-01 00:18:32 +0800

    cleanup indentation and logging

--- src/config/config.js
+++ src/config/config.js
@@ -17,53 +17,50 @@ class GitstatsConfigCtrl {
   }
 
   GetTasks() {
-  	var self = this;
+    var self = this;
 
-  	this.backendSrv.get("/api/plugin-proxy/gitstats-app/api/tasks", {metric: "/raintank/apps/gitstats/*/*/*"}).then(function(resp) {
-  		console.log("getTasks resp: %j", resp);
-  		self.tasks = resp;
-  	});
+    this.backendSrv.get("/api/plugin-proxy/gitstats-app/api/tasks", {metric: "/raintank/apps/gitstats/*/*/*"}).then(function(resp) {
+      self.tasks = resp;
+    });
   }
 
   addNewTask() {
-  	var self = this;
-  	var task = {
-	    "name": "Github Stats: " + this.newTask.owner + "/"+this.newTask.repo,
-	    "metrics": {"/raintank/apps/gitstats/*/*/*":1},
-	    "config": {
-	        "/raintank/apps/gitstats": {
-	        	"repo": this.newTask.repo,
-	        	"owner": this.newTask.owner,
-	        	"access_token": this.newTask.access_token
-	        }
-	    },
-	    "interval": 60,
-	    "route": { "type": "any", "config": {}},
-	    "enabled": true
-	};
+    var self = this;
+    var task = {
+      "name": "Github Stats: " + this.newTask.owner + "/"+this.newTask.repo,
+      "metrics": {"/raintank/apps/gitstats/*/*/*":1},
+      "config": {
+        "/raintank/apps/gitstats": {
+          "repo": this.newTask.repo,
+          "owner": this.newTask.owner,
+          "access_token": this.newTask.access_token
+        }
+      },
+      "interval": 60,
+      "route": { "type": "any", "config": {}},
+      "enabled": true
+    };
 
-  	this.backendSrv.post("/api/plugin-proxy/gitstats-app/api/tasks", task).then(function(resp) {
-  		console.log("New task Created: %j", resp);
-  		self.GetTasks();
-  		self.cancelNewTask();
-  	});
+    this.backendSrv.post("/api/plugin-proxy/gitstats-app/api/tasks", task).then(function(resp) {
+      self.GetTasks();
+      self.cancelNewTask();
+    });
   }
 
   removeTask(task) {
-  	var self = this;
-  	this.backendSrv.delete("/api/plugin-proxy/gitstats-app/api/tasks/"+task.id).then(function(resp) {
-  		self.GetTasks();
-  	});
+    var self = this;
+    this.backendSrv.delete("/api/plugin-proxy/gitstats-app/api/tasks/"+task.id).then(function(resp) {
+      self.GetTasks();
+    });
   }
 
   cancelNewTask() {
-  	this.newTask = {};
-  	this.addNew = false;
+    this.newTask = {};
+    this.addNew = false;
   }
 
   toggleAddNew() {
-  	console.log("addNew");
-  	this.addNew = true;
+    this.addNew = true;
   }
 }
 
